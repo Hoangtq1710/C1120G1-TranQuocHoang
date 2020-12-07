5ab9182795feb3e1a1c1c7a64d4a20f06ea20c33
TranQuocHoang <hoangtq1710@gmail.com>
2020-12-07 17:03:27 +0700

    commit Bài 7

--- Bài 7/Bài tập/[Bài tập] Ứng dụng chuyển đổi tiền tệ/switch_currency.js
+++ Bài 7/Bài tập/[Bài tập] Ứng dụng chuyển đổi tiền tệ/switch_currency.js
@@ -1,31 +1,72 @@
+// let from_cur = document.getElementById("from_cur");
+// let to_cur = document.getElementById("to_cur");
+// let from_cur;
+// let from;
+// let to;
+// let to_cur;
+// let result;
+// let input_amount = document.getElementById("amount");
+//
+// function convert() {
+//
+//
+//     let amount = parseFloat(input_amount.value);
+//
+//     const vnd = 1;
+//     const usd = 23000;
+//     const eur = 25000;
+//
+//
+//     // switch ("from_currency") {
+//     //     case 1: from_cur = vnd; break;
+//     //     case 2: from_cur = usd; break;
+//     //     case 3: from_cur = eur; break;
+//     // }
+//     //
+//     // switch ("to_currency") {
+//     //     case 4: to_cur = vnd; break;
+//     //     case 5: to_cur = usd; break;
+//     //     case 6: to_cur = eur; break;
+//     // }
+//
+//     from_cur = document.getElementById("from_cur");
+//     to_cur = document.getElementById("to_cur");
+//
+// //     switch (from_cur) {
+// //         case 1: from_cur.value = vnd;
+// //         case 2: from_cur.value = usd;
+// //         case 3: from_cur.value = eur;
+// //     }
+// //     switch (to_cur) {
+// //         case 1: to_cur.value = vnd;
+// //         case 2: to_cur.value = usd;
+// //         case 3: to_cur.value = eur;
+// //     }
+// //
+// //
+// //
+// //
+// //     result = amount * from_cur / to_cur ;
+// //
+// //     document.getElementById("result").innerText = result;
+// //
+// //     console.log(result);
+// //     // document.write(result);
+// //
+// //
+// // //         ví dụ 20 , USD sand VND => result = 20*23000/1;
+// // //         ví dụ 75000 , VND sang EUR => result = 75000*1/25000;
+// // //         ví dụ 15 , EUR sang USD => result = 15 * 25000/23000;
+// }
+let a = document.getElementById("amount").value
+let b = document.getElementById("from_cur").value
+let c = document.getElementById("to_cur").value
+let l;
+
 function convert() {
-    let amount = document.getElementById(amount);
-
-    let vnd = 1;
-    let usd = 23000;
-    let eur = 25000;
-
-    let from_cur ;
-    let to_cur ;
-
-    switch (document.getElementById(from_cur)) {
-        case 1: from_cur = vnd; break;
-        case 2: from_cur = usd; break;
-        case 3: from_cur = eur; break;
-    }
-
-    switch (document.getElementById(to_cur)) {
-        case 1: to_cur = vnd; break;
-        case 2: to_cur = usd; break;
-        case 3: to_cur = eur; break;
-    }
-
-     let result = amount * from_cur / to_cur ;
-
-    document.write(result);
-
-
-//         ví dụ 20 , USD sand VND => result = 20*23000/1;
-//         ví dụ 75000 , VND sang EUR => result = 75000*1/25000;
-//         ví dụ 15 , EUR sang USD => result = 15 * 25000/23000;
+    if (b == "VND" && c == "USA") {
+        l = a / 23000
+    } else if (b=="USA" && c == "VND")
+        l = a * 23000
 }
+document.getElementById("result").innerText=l;

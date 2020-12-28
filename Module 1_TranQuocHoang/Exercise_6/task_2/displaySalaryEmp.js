24867b87d2e1f970a2e1546672fe2bb89420b131
TranQuocHoang <hoangtq1710@gmail.com>
2020-12-28 11:16:31 +0700

    Commit Case Study Furama

--- Module 1_TranQuocHoang/Exercise_6/task_2/displaySalaryEmp.js
+++ Module 1_TranQuocHoang/Exercise_6/task_2/displaySalaryEmp.js
@@ -1,17 +1,16 @@
 function displaySalaryEmp() {
     let flag_idEm = true;
-    let input_emId = parseInt(prompt("Enter ID of Employee you wanna show Salary"));
+    let input_emId = prompt("Enter ID of Employee you wanna show Salary");
     for (let i = 0; i < arrListEmp.length; i++) {
-        if (input_emId === arrListEmp[i].getEmId()) {
+        if (input_emId == arrListEmp[i].getEmId()) {
             index = i;
             flag_idEm = false;
-        } else {
-            alert("Wrong ID!!!");
         }
-
-        if (flag_idEm === false) {
-            display();
-        }
+    }
+    if (flag_idEm === false) {
+        display();
+    } else {
+        alert("Wrong ID");
     }
 }
 function display() {

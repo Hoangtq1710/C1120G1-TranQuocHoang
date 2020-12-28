e63c859bad1f17027a7e7ccdf035c0bb98a70193
TranQuocHoang <hoangtq1710@gmail.com>
2020-12-28 11:02:58 +0700

    Commit Case Study Furama

--- Module 1_TranQuocHoang/Exercise_6/task_2/displaySalaryEmp.js
+++ Module 1_TranQuocHoang/Exercise_6/task_2/displaySalaryEmp.js
@@ -1,15 +1,19 @@
 function displaySalaryEmp() {
     let flag_idEm = true;
-    let input_emId = prompt("Enter ID of Employee you wanna show Salary");
+    let input_emId = parseInt(prompt("Enter ID of Employee you wanna show Salary"));
     for (let i = 0; i < arrListEmp.length; i++) {
-        for (let j = 0; j < arrListEmp[i].length; j++) {
-            if (input_emId === arrListEmp[i].getEmId()) {
-                index = i;
-                flag_idEm = false;
-            }
+        if (input_emId === arrListEmp[i].getEmId()) {
+            index = i;
+            flag_idEm = false;
+        } else {
+            alert("Wrong ID!!!");
         }
+
         if (flag_idEm === false) {
-            alert("Employee "+arrListEmp[index].getEmName()+" has salary is "+arrListEmp[index].getSalary());
+            display();
         }
     }
+}
+function display() {
+    alert("Employee "+arrListEmp[index].getEmName()+" has salary is "+arrListEmp[index].getSalary());
 }

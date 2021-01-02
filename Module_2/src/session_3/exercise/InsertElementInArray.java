d3f529efb28a65c697bd610878b7627f2d19f184
TranQuocHoang <hoangtq1710@gmail.com>
2021-01-02 23:24:58 +0700

    Commit Module 2 Bai 3

--- Module_2/src/session_3/exercise/InsertElementInArray.java
+++ Module_2/src/session_3/exercise/InsertElementInArray.java
@@ -11,13 +11,18 @@ public class InsertElementInArray {
         int insert;
         int index;
 
-        System.out.println("Enter the size of Array (0 <= size <= 50) :");
+        System.out.println("Enter the size of Array (0 < size <= 50) :");
         do {
             size = scanner.nextInt();
-        } while ((size < 0) || (size > 50));
-        array = new int[size];
+            if ((size <= 0) || (size > 50)) {
+                System.out.print("Invalid size. Enter again!\n");
+            }
+        } while ((size <= 0) || (size > 50));
+
+        array = new int[size +1];
+
         for (int i = 0; i < size; i++) {
-            System.out.println("Element "+ (i+1)+" :");
+            System.out.println("Element "+i+" :");
             array[i] = scanner.nextInt();
         }
         System.out.println("Enter the Element you wanna insert :");
@@ -25,17 +30,20 @@ public class InsertElementInArray {
         System.out.println("Enter the index (0 <= index <= "+(size-1)+") :");
         do {
             index = scanner.nextInt();
+            if ((index < 0) || (index >= size)) {
+                System.out.print("Wrong index. Input again!\n");
+            }
         } while ((index < 0) || (index >= size));
 
         System.out.print("Array :");
-        for(int value : array) {
-            System.out.print(" "+value);
+        for (int i = 0; i < array.length -1; i++) {
+            System.out.print(" "+array[i]);
         }
         System.out.println("\n");
 
         for (int i = 0; i < array.length; i++) {
             if (i == index) {
-                for (int j = array.length; j > i+1 ; j--) {
+                for (int j = array.length -1; j > i ; j--) {
                     array[j] = array[j-1];
                 }
                 array[i] = insert;

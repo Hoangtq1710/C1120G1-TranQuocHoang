552d04405d032cca56f159d8c6793df93b802708
Hoangtq1710 <hoangtq1710@gmail.com>
2021-01-19 14:49:31 +0700

    "Commit Module 2 Bai 11"

--- Module_2/src/session_11/exercise/optional_queue_linked_list_circle/Queue.java
+++
@@ -1,4 +0,0 @@
-package session_11.exercise.optional_queue_linked_list_circle;
-
-public class Queue {
-}
---
+++ Module_2/src/session_11/exercise/optional_queue_linked_list_circular/Queue.java
@@ -0,0 +1,6 @@
+package session_11.exercise.optional_queue_linked_list_circular;
+
+public class Queue {
+    Node front;
+    Node rear;
+}

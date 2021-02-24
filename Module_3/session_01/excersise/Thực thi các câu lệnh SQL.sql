1d6ec2ff9cc3080f9e8f178c712b33ffe9eaa7e1
Hoangtq1710 <hoangtq1710@gmail.com>
2021-02-24 14:11:23 +0700

    Commit Module 3 Bai 1

---
+++ Module_3/session_01/excersise/Thực thi các câu lệnh SQL.sql
@@ -0,0 +1,5 @@
+SELECT * FROM classicmodels.customers;
+SELECT * FROM customers WHERE city = 'Nantes';
+SELECT * FROM customers WHERE customername like '%A%g%a%s%';
+SELECT phone, city, customerName, customerNumber FROM customers;
+

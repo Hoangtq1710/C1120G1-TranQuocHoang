f1b38d6cb7cea361f83c2b942ef143249f9dd16a
Hoangtq1710 <hoangtq1710@gmail.com>
2021-03-13 14:53:55 +0700

    Commit Module 3 Bai 11

--- Module_3/session_11/practice/customer-management-application/src/practice_customer_management/service/CustomerServiceImpl.java
+++ Module_3/session_11/practice/customer-management-application/src/practice_customer_management/service/CustomerServiceImpl.java
@@ -3,44 +3,46 @@ package practice_customer_management.service;
 import practice_customer_management.model.Customer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private static Map<Integer, Customer> customerList ;
+    private static Map<Integer, Customer> customers;
 
     static  {
-        customerList.put(1,new Customer(1,"Quang","quang@gmail.com","Da Nang"));
-        customerList.put(2,new Customer(2,"Dong","dong@gmail.com","Hue"));
-        customerList.put(3,new Customer(3,"Ngoc","ngoc@gmail.com","Quang Tri"));
-        customerList.put(4,new Customer(4,"Hoang","hoang@gmail.com","Quang Nam"));
-        customerList.put(5,new Customer(5,"Thuan","thuan@gmail.com","Hue"));
-        customerList.put(6,new Customer(6,"Vi","vi@gmail.com","Da Nang"));
+        customers = new HashMap<>();
+        customers.put(1,new Customer(1,"Quang","quang@gmail.com","Da Nang"));
+        customers.put(2,new Customer(2,"Dong","dong@gmail.com","Hue"));
+        customers.put(3,new Customer(3,"Ngoc","ngoc@gmail.com","Quang Tri"));
+        customers.put(4,new Customer(4,"Hoang","hoang@gmail.com","Quang Nam"));
+        customers.put(5,new Customer(5,"Thuan","thuan@gmail.com","Hue"));
+        customers.put(6,new Customer(6,"Vi","vi@gmail.com","Da Nang"));
     }
 
     @Override
     public List<Customer> findAll() {
-        return new ArrayList<>(customerList.values());
+        return new ArrayList<>(customers.values());
     }
 
     @Override
     public void save(Customer customer) {
-        customerList.put(customer.getId(), customer);
+        customers.put(customer.getId(), customer);
     }
 
     @Override
     public Customer findById(int id) {
-        return customerList.get(id);
+        return customers.get(id);
     }
 
     @Override
     public void update(int id, Customer customer) {
-        customerList.put(id,customer);
+        customers.put(id,customer);
     }
 
     @Override
     public void remove(int id) {
-        customerList.remove(id);
+        customers.remove(id);
     }
 }

491ed6dccc6606087df5b01e377b36c72da2f7cc
Hoangtq1710 <hoangtq1710@gmail.com>
2021-04-01 18:18:11 +0700

    Commit Module 4 Bai 4

--- Module_4/session_04_thymeleaf/practice-user-management/src/main/java/com/soren/controller/CustomerController.java
+++ Module_4/session_04_thymeleaf/practice-user-management/src/main/java/com/soren/controller/CustomerController.java
@@ -4,10 +4,12 @@ import com.soren.model.Customer;
 import com.soren.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -17,7 +19,7 @@ public class CustomerController {
     @Autowired
     CustomerService customerService;
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public ModelAndView getHome(){
         List<Customer> listCustomer = this.customerService.findAll();
         return new ModelAndView("index","listCustomer",listCustomer);
@@ -46,7 +48,7 @@ public class CustomerController {
     @GetMapping("/show-edit")
     public ModelAndView showEditForm(@RequestParam int id){
         Customer customer = this.customerService.findById(id);
-        return new ModelAndView("show-edit", "customer",customer);
+        return new ModelAndView("edit", "customer",customer);
     }
 
     @PostMapping("/edit-customer")
@@ -54,6 +56,21 @@ public class CustomerController {
         this.customerService.update(customer.getId(), customer);
         modelAndView.addObject("messageUpdate","Customer information was updated");
         return getHome();
+    }
+
+    @GetMapping("/show-delete")
+    public String showDeleteForm(@RequestParam int id, Model model){
+        Customer customer = this.customerService.findById(id);
+        model.addAttribute("customer", customer);
+        model.addAttribute("confirm", "ARE YOU SURE TO DELETE CUSTOMER WITH ID = "+customer.getId()+" ?");
+        return "delete";
+    }
+
+    @PostMapping("/delete")
+    public String deleteCustomer(Customer customer, RedirectAttributes redirect){
+        this.customerService.remove(customer.getId());
+        redirect.addFlashAttribute("message", "Customer was deleted");
+        return "redirect:/home";
     }
 
 }

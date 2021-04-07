82024ff30a0db9d5a9fb5bb97eec15585a147217
Hoangtq1710 <hoangtq1710@gmail.com>
2021-04-07 17:30:25 +0700

    Commit Module 4 Bai 7

--- Module_4/session_07_spring_data_repository/exercise_blog_upgrade/src/main/java/com/soren/ss07_blog_upgrade/controller/BlogController.java
+++ Module_4/session_07_spring_data_repository/exercise_blog_upgrade/src/main/java/com/soren/ss07_blog_upgrade/controller/BlogController.java
@@ -6,7 +6,9 @@ import com.soren.ss07_blog_upgrade.service.BlogService;
 import com.soren.ss07_blog_upgrade.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,10 +32,11 @@ public class BlogController {
     CategoryService categoryService;
 
     @GetMapping("")
-    public String getHomeAdmin(Model model,
-                               @PageableDefault(value = 5) Pageable pageable){
-        Page<Blog> listBlog = this.blogService.findAll(pageable);
+    public String getHomeAdmin(Model model,Pageable pageable){
+        Pageable pageableSortByPostDay = PageRequest.of(pageable.getPageNumber(), 3, Sort.by("postDay").descending());
+        Page<Blog> listBlog = this.blogService.findAll(pageableSortByPostDay);
         List<Category> listCategory = this.categoryService.findAll();
+        model.addAttribute("reverseSortBy", "asc");
         model.addAttribute("listBlog", listBlog);
         model.addAttribute("listCategory", listCategory);
         model.addAttribute("blog", new Blog());
@@ -55,8 +58,7 @@ public class BlogController {
     }
 
     @GetMapping("/show_edit")
-    public String showEditForm(@RequestParam Integer id, Model model,
-                               @PageableDefault(value = 3) Pageable pageable){
+    public String showEditForm(@RequestParam Integer id, Model model){
         Blog blog = this.blogService.findById(id);
         List<Category> listCategory = this.categoryService.findAll();
         model.addAttribute("listCategory", listCategory);
@@ -83,11 +85,12 @@ public class BlogController {
                                 @PageableDefault(value = 3) Pageable pageable,
                                 Model model){
         Page<Blog> listBlog;
+        Pageable pageableSortByPostDay = PageRequest.of(pageable.getPageNumber(), 3, Sort.by("postDay").descending());
         if (search.isPresent()){
-            listBlog = this.blogService.findAllByTitleContaining(search.get(), pageable);
+            listBlog = this.blogService.findAllByTitleContaining(search.get(), pageableSortByPostDay);
             model.addAttribute("search", search.get());
         } else {
-            listBlog = this.blogService.findAll(pageable);
+            listBlog = this.blogService.findAll(pageableSortByPostDay);
         }
         model.addAttribute("listBlog", listBlog);
 
@@ -95,6 +98,23 @@ public class BlogController {
 
         model.addAttribute("blog", new Blog());
         model.addAttribute("listCategory", listCategory);
+        return "admin";
+    }
+
+    @GetMapping("/sort")
+    public String sortBlogListByPostDay(@RequestParam String sortBy,Pageable pageable, Model model){
+        Page<Blog> listBlog;
+        Pageable pageableSortByPostDay;
+        if (sortBy.equals("asc")){
+            pageableSortByPostDay = PageRequest.of(pageable.getPageNumber(), 3, Sort.by("postDay").ascending());
+        } else {
+            pageableSortByPostDay = PageRequest.of(pageable.getPageNumber(), 3, Sort.by("postDay").descending());
+        }
+        listBlog = this.blogService.findAll(pageableSortByPostDay);
+
+        model.addAttribute("reverseSortBy", sortBy.equals("asc") ? "desc" : "asc");
+        model.addAttribute("listBlog", listBlog);
+        model.addAttribute("blog", new Blog());
         return "admin";
     }
 }

ce327501dcf4526009623234d65e8b8d215550cc
Hoangtq1710 <hoangtq1710@gmail.com>
2021-04-10 15:40:55 +0700

    Commit Module 4 Bai 9

--- Module_4/session_09_aop_handle_exception/exercise_borrow_book_app/src/main/java/com/soren/concern/Logger.java
+++ Module_4/session_09_aop_handle_exception/exercise_borrow_book_app/src/main/java/com/soren/concern/Logger.java
@@ -2,13 +2,29 @@ package com.soren.concern;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class Logger {
 
-    @After("execution(* com.soren.service.BookServiceImpl.findAll(..))")
-    public void afterFindAll(JoinPoint joinPoint){
-        System.err.println("Sau khi ket thuc ham : "+joinPoint.getSignature().getName());
+    @After("execution(* com.soren.controller.BookController.getHome(..))")
+    public void afterGetHome(JoinPoint joinPoint){
+        System.err.println("Sau khi ket thuc ham getHome : "+joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* com.soren.controller.BookController.borrowBook(..))")
+    public void afterBorrowBook(JoinPoint joinPoint) {
+        System.err.println("Sau khi goi ham : " + joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* com.soren.controller.BookController.givebackBook(..))")
+    public void afterGivebackBook(JoinPoint joinPoint) {
+        System.err.println("Sau khi goi ham : " + joinPoint.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.soren.service.BookServiceImpl.decreasingBookQuantity(..))")
+    public void afterThrowingException(JoinPoint joinPoint) {
+        System.err.println("Sau khi throw Exception : " + joinPoint.getSignature().getName());
     }
 }

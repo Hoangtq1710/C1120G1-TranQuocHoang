63f59e43700c99b0d00d635c3ef2bb9292ae9b96
Hoangtq1710 <hoangtq1710@gmail.com>
2021-05-06 19:00:28 +0700

    Commit Module 5 Bai 3

--- Module_5/session_03/exercise/color-picker/src/app/color-picker/color-picker.component.ts
+++ Module_5/session_03/exercise/color-picker/src/app/color-picker/color-picker.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ColorPickerComponent implements OnInit {
   public title = 'Color Picker';
 
-  public colorCode:string;
+  public colorCode:string = "#387c73";
 
   constructor() { }
 
